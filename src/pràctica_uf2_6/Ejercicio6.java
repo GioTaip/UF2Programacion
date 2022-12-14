00eed3a267e48fee4cd5d0a4b7252720becae73e
GioTaipe <a22martiptai@inspedralbes.cat>
2022-12-14 21:07:23 +0100

    ejercicio6 finalizado

--- src/pràctica_uf2_6/Ejercicio6.java
+++ src/pràctica_uf2_6/Ejercicio6.java
@@ -15,6 +15,17 @@ public class Ejercicio6 {
      */
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+        System.out.print("Introduce la frase: ");
+        String frase = scan.nextLine();
+        System.out.println(Iniciales(frase));
+    }
+    public static String Iniciales(String frase){
+        String[] palabras = frase.split(" ");
+        String iniciales = "";
+        for(int i = 0; i < palabras.length; i++){
+            iniciales += palabras[i].substring(0,1).toUpperCase();
+        }
+        return iniciales;
     }
     
 }
