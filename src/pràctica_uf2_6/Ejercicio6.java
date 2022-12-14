c5432f23a8da5752292e37a6afa00612938ec759
GioTaipe <a22martiptai@inspedralbes.cat>
2022-12-14 20:37:41 +0100

    añadir ejercicio6

---
+++ src/pràctica_uf2_6/Ejercicio6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pràctica_uf2_6;
+import java.util.Scanner;
+/**
+ *
+ * @author giiov
+ */
+public class Ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+    }
+    
+}
