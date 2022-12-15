7307f59adea7f67b3f1ac5aa868767bb8da9cfe5
GioTaipe <a22martiptai@inspedralbes.cat>
2022-12-15 23:49:39 +0100

    Terminado

--- src/pràctica_uf2_6/Ejercicio1.java
+++ src/pràctica_uf2_6/Ejercicio1.java
@@ -15,17 +15,23 @@ public class Ejercicio1 {
      */
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        
+        // Solicitamos el numero por teclado
         System.out.print("Numero: ");
         int Nmiembro=scan.nextInt();
+        // Mostramos por pantalla el nombre de la lista haciendo una llamada a nuestra función
         System.out.println(QuienEsIntegranteDeEsteGrupo( Nmiembro));
         
-    }
+    } /* Creamos nuestra función llamada "QuienEsIntegranteDeEsteGrupo" la definimos con una variable
+         String ya que es eso lo que nos devolvera recibiendo un Entero*/
     public static String QuienEsIntegranteDeEsteGrupo(int Nmiembro){
-       String[] miembros={"Marta","Juan"};
+       // Creamos una variable String con un Array con nuestra lista definida
+       String[] miembros={"Geovanny","Jonathan"}; 
+       /* Condicionamos la entrada para indicar si el entero que entra esta dentro del array y devolvemos el nombre
+          ya que el numero que entra es el orden de la lista enumerada*/
        if(Nmiembro-1 >= 0 && Nmiembro-1 < miembros.length){
            return miembros[Nmiembro-1];
        }
+       // En caso de que no se cumpla la condición y el numero que entre este fuera del array nos devolvera una cadena vacia
        return "";
     }
     
--- src/pràctica_uf2_6/Ejercicio6.java
+++ src/pràctica_uf2_6/Ejercicio6.java
@@ -15,13 +15,21 @@ public class Ejercicio6 {
      */
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+        // Pedimos por teclado la frase
         System.out.print("Introduce la frase: ");
         String frase = scan.nextLine();
+        // Mostramos por pantalla las SIGLAS de la frase introducida haciendo una llamada a nuestra función
         System.out.println(Iniciales(frase));
     }
+    // Creamos nuestra función llamada "Iniciales" definida con un String recibiendo otro String
     public static String Iniciales(String frase){
-        String[] palabras = frase.split(" ");
+        /* Creamos las varibles a ulizar, la primera sera con un array y la segunda sera donde almacenaremos nuestras SIGLAS.
+        Para detectar el numero de palabras que hay en la frase utilizaremos un split 
+        y utilizaremos los espacios para indicar el inicio de una  nueva palabra*/
+        String[] palabras = frase.split(" ");                                               
         String iniciales = "";
+        /* Creamos un bucle para recorrer la frase y utilizando el "substring" indicaremos que parte de la pabra nos coja 
+        por ultimo lo convertimos en mayusculas*/
         for(int i = 0; i < palabras.length; i++){
             iniciales += palabras[i].substring(0,1).toUpperCase();
         }
